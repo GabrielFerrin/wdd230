b2549dc3873b2ff9a1c154615674ff8972758494
agferrin <39132131+agferrin@users.noreply.github.com>
2023-05-16 22:17:12 -0500

    w04 update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,52 +1,16 @@
-// date
-const date = new Date()
-const day = date.getDate();
-const month = date.getMonth() + 1;
-const year = date.getFullYear();
-
-const dayNames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-
-const monthNames = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-
-const dayName = dayNames[date.getDay()]
-const monthName = monthNames[date.getMonth()]
-
-const completeDate = `${dayName}, ${day} ${monthName} ${year}`;
+const now = new Date();
+const nowDate = new Intl.DateTimeFormat("en-US", {
+	dateStyle: "full"
+}).format(now);
 document.getElementById('header-date')
-  .innerText = completeDate;
+	.innerText = nowDate;
+
+// copyright
+const copyrightDate = new Date(document.lastModified);
+document.getElementById('copyright')
+	.innerText = '©' + copyrightDate.getFullYear() +
+	' Manta Chamber';
 
 // last modified
-const lastModified = new Date(document.lastModified);
-const lastModifiedMonth = ('0' + lastModified.getMonth()).slice(-2);
-const lastModifiedDay = ('0' + lastModified.getDay()).slice(-2);
-const lastModifiedHour = ('0' + lastModified.getHours()).slice(-2);
-const lastModifiedMinute = ('0' + lastModified.getMinutes()).slice(-2);
-const lastModifiedSecond = ('0' + lastModified.getSeconds()).slice(-2);
-const lastModifiedDate = 'Last modification: ' + lastModifiedDay +
-  '/' + lastModifiedMonth + '/' + lastModified.getFullYear() +
-  '/' + lastModifiedHour + ':' + lastModified.getMinutes() + ':' +
-  lastModified.getSeconds();
-  
 document.getElementById('last-modified')
-  .innerText = lastModifiedDate;
+	.innerText = 'Last Modification: ' + document.lastModified;
