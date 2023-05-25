945cf3fc747bd4d40c2bcc923fa18217f4b13e4e
agferrin <39132131+agferrin@users.noreply.github.com>
2023-05-25 13:28:41 -0500

    w05 update

---
+++ lesson05/scripts/script.js
@@ -0,0 +1,22 @@
+const input = document.getElementById('favchap');
+const button = document.querySelector('button');
+const list = document.getElementById('list');
+
+button.addEventListener('click', () => {
+	if (input.value != '') {
+		li = document.createElement('li');
+		deleteButton = document.createElement('button');
+		li.innerText = input.value;
+		deleteButton.innerText = '❌';
+		deleteButton.classList.add('delete');
+		deleteButton.ariaLabel = `Remove: ${input.innerText}`;
+		deleteButton.addEventListener('click', (event) => {
+			event.currentTarget.parentNode.remove();
+			input.focus();
+		})
+		li.append(deleteButton);
+		list.append(li);
+		input.focus();
+		input.value = '';
+	}
+});
