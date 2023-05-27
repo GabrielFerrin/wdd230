7fed834b724ff331b533f610e856c4a1a3bb1c5c
agferrin <39132131+agferrin@users.noreply.github.com>
2023-05-26 23:27:23 -0500

    w05 update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,33 +1,43 @@
 // hamburger menu
 let toggleMenu = _ => {
 	// menu button
-  const menuButton = document.getElementById('menu-button');
+	const menuButton = document.getElementById('menu-button');
 	menuButton.classList.toggle('opened');
+	menuButton.setAttribute('aria-expanded', menuButton.classList
+		.contains('opened'));
 
 	document.getElementsByClassName('header-menu')[0]
 		.classList.toggle('responsive');
 
 	// background when menu is active
 	const isBGOn = document.getElementsByClassName('menu-background')[0]
-	.style.display == "block";
+		.style.display == "block";
+	document.getElementsByClassName('menu-background')[0]
+		.style.display = isBGOn ? "none" : "block";
+
 	document.getElementsByClassName('menu-background')[0]
 		.style.display = isBGOn ? "none" : "block";
 }
 
 // hide menu background
 let hideMenuBG = _ => {
-	document.getElementsByClassName('menu-background')[0]
-		.style.display = "none";
-	document.getElementsByClassName('header-menu')[0]
-		.classList.toggle('responsive');
+
 }
 
+// date
 const now = new Date();
 const nowDate = new Intl.DateTimeFormat("en-US", {
 	dateStyle: "full"
 }).format(now);
 document.getElementById('header-date')
 	.innerText = nowDate;
+
+// show banner Mondays and Tesdays
+if (now.getDay() == 1 || now.getDay() == 2) {
+	setTimeout(() => {
+		document.getElementById('announcement-banner').style.display = "flex";
+	}, 1000);
+}
 
 // copyright
 const copyrightDate = new Date(document.lastModified);
