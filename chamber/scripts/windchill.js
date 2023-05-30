afeb0136aa51cb1ee59d18fed9fd2d1d5e18a7d5
agferrin <39132131+agferrin@users.noreply.github.com>
2023-05-30 16:24:25 -0500

    w06 update

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,26 @@
+const celciusTemperature = document.getElementById('temperature').innerText;
+const speedKilometers = document.getElementById('wind-speed').innerText;
+
+// Celsius to Fahrenheit
+const fahrenheitTemperature = (celciusTemperature * 1.8) + 32;
+// Kilometers per hour
+const speedMiles = speedKilometers / 1.609;
+
+// wind chill
+let windchill;
+if (fahrenheitTemperature <= 50 && speedMiles > 3) {
+  windchill =  35.74 + (0.6215 * fahrenheitTemperature) -
+    (35.75 * Math.pow(speedMiles, 0.16)) + (0.4275 * fahrenheitTemperature *
+    Math.pow(speedMiles, 0.16));
+    console.log(windchill);
+  windchill = ((windchill - 32) * (5 / 9)).toFixed(0) + ' °C';
+  console.log(windchill);
+} else {
+  windchill = 'N/A';
+}
+
+// Fahrenheit to Celsius
+
+document.getElementById('windchill').innerText = windchill;
+
+
