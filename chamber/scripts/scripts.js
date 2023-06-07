1a63dcaa423e19e64911aa364202d3486fd7afe6
agferrin <39132131+agferrin@users.noreply.github.com>
2023-06-06 22:34:03 -0500

    w07 update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,27 +1,25 @@
 // hamburger menu
 let toggleMenu = _ => {
 	// menu button
-	const menuButton = document.getElementById('menu-button');
+	const menuButton = document
+		.getElementById('menu-button');
 	menuButton.classList.toggle('opened');
-	menuButton.setAttribute('aria-expanded', menuButton.classList
-		.contains('opened'));
+	menuButton
+		.setAttribute('aria-expanded', menuButton.classList
+			.contains('opened'));
 
 	document.getElementsByClassName('header-menu')[0]
 		.classList.toggle('responsive');
 
 	// background when menu is active
-	const isBGOn = document.getElementsByClassName('menu-background')[0]
+	const isBGOn = document
+		.getElementsByClassName('menu-background')[0]
 		.style.display == "block";
 	document.getElementsByClassName('menu-background')[0]
 		.style.display = isBGOn ? "none" : "block";
 
 	document.getElementsByClassName('menu-background')[0]
 		.style.display = isBGOn ? "none" : "block";
-}
-
-// hide menu background
-let hideMenuBG = _ => {
-
 }
 
 // date
@@ -34,7 +32,8 @@ document.getElementById('header-date')
 
 // show banner Mondays and Tesdays
 if (now.getDay() == 1 || now.getDay() == 2) {
-	document.getElementById('announcement-banner').style.display = "flex";
+	document.getElementById('announcement-banner')
+		.style.display = "flex";
 }
 
 // copyright
@@ -45,7 +44,8 @@ document.getElementById('copyright')
 
 // last modified
 document.getElementById('last-modified')
-	.innerText = 'Last Modification: ' + document.lastModified;
+	.innerText = 'Last Modification: '
+	+ document.lastModified;
 
 const copyrightLine = '©' + copyrightDate.getFullYear()
 	+ 'Manta Chamber | Gabriel Ferrin M. | WDD 203 Project '
@@ -55,19 +55,95 @@ document.getElementById('copyright-line')
 	.innerText = copyrightLine;
 
 // increase number animation
-const increaseNumbers = document.querySelectorAll('.increase-number');
+const increaseNumbers = document
+	.querySelectorAll('.increase-number');
 
 increaseNumbers.forEach((number) => {
-	let value = 0;
-	const endNumber = Number(number.getAttribute('number-info')).toFixed(0);
-	const increase = endNumber / 50;
-	let counter = setInterval(() => {
-		number.innerText = value.toFixed(2);
-		value += increase;
-			if (value >= endNumber) {
-				clearInterval(counter);
-				number.innerText = 
-					Number(number.getAttribute('number-info')).toLocaleString();
-			}
-	}, 25);
+
 });
+
+// lazy loading images and increase numbers effect
+const imagesToLoad = document.querySelectorAll('[data-image]');
+const numbers = document.querySelectorAll('.increase-number');
+
+// make sure we are on "discover page"
+if (imagesToLoad.length) {
+	// images threshold
+	const imageOptions = {
+		threshold: 0
+	}
+	// numbers threshold
+	const numberOptions = {
+		threshold: 0.8
+	}
+
+	// function for loading image
+	const loadImage = (image) => {
+		image.setAttribute('src', image.getAttribute('data-image'));
+		image.onload = () => {
+			image.removeAttribute('data-image')
+		}
+	}
+
+	// intersection observers
+	if ("IntersectionObserver" in window) {
+		// imagery lazy loading
+		const imageObserver =
+			new IntersectionObserver((items, observer) => {
+				items.forEach((item) => {
+					if (item.isIntersecting) {
+						loadImage(item.target);
+						observer.unobserve(item.target);
+					}
+				})
+			}, imageOptions);
+
+		imagesToLoad.forEach((image) => {
+			imageObserver.observe(image);
+		});
+
+		// increase number effect
+		const triggerEffect = (number) => {
+			let counter = 0;
+			const endNumber =
+				Number(number.getAttribute('number-info')).toFixed(0);
+			const increase = endNumber / 50;
+			let interval = setInterval(() => {
+				number.innerText = counter.toFixed(1);
+				counter += increase;
+				if (counter >= endNumber) {
+					clearInterval(interval);
+					number.innerText =
+						Number(number.getAttribute('number-info'))
+							.toLocaleString();
+				}
+			}, 8);
+		}
+
+		const numberObserver =
+			new IntersectionObserver((items, observer) => {
+				items.forEach((number) => {
+					if (number.isIntersecting) {
+						triggerEffect(number.target);
+					} else {
+						number.target.innerText = 0;
+					}
+				});
+			}, numberOptions);
+
+		numbers.forEach((number) => {
+			numberObserver.observe(number);
+		});
+	} else {
+		imagesToLoad.forEach((image) => {
+			loadImage(image);
+		});
+
+		numbers.forEach((number) => {
+			triggerEffect(number);
+		});
+	}
+}
+
+
+
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -12,9 +12,7 @@ if (fahrenheitTemperature <= 50 && speedMiles > 3) {
   windchill =  35.74 + (0.6215 * fahrenheitTemperature) -
     (35.75 * Math.pow(speedMiles, 0.16)) + (0.4275 * fahrenheitTemperature *
     Math.pow(speedMiles, 0.16));
-    console.log(windchill);
   windchill = ((windchill - 32) * (5 / 9)).toFixed(0) + ' °C';
-  console.log(windchill);
 } else {
   windchill = 'N/A';
 }
