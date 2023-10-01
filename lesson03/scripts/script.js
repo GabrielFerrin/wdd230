53dddf2b8aa3825078e6e03109a374c49a8fcd14
GabrielFerrin <gabrielferrin@byui.edu>
2023-10-01 04:04:35 -0500

    site-plan w3

--- lesson03/scripts/script.js
+++ lesson03/scripts/script.js
@@ -1,8 +1,7 @@
 const frame = document.getElementById('iphone-frame');
-
 // phone frame observer
 const frameObserver = new ResizeObserver(entries => {
-  const wrapper = document.getElementsByClassName('mobile-wireframe');
+  const wrapper = document.getElementsByClassName('mobile-card');
   wrapper[0].style.height = entries[0].contentRect.height + 'px';
 });
 frameObserver.observe(frame);
