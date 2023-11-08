a3e6294ee403b31144814ea31ca56263b47e7536
GabrielFerrin <gabrielferrin@byui.edu>
2023-11-08 15:04:38 -0500

    JSON-menu w09

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,12 @@
-const baseURL = 'https://gabrielferrin.github.io/wdd230/data/links.json';
+const baseURL = 'https://gabrielferrin.github.io/wdd230/';
+const linksURL = 'https://gabrielferrin.github.io/wdd230/data/links.json';
+const getLinks = async () => {
+  const response = await fetch(linksURL);
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data.lessons);
+  }
+  return undefined;
+}
+
+getLinks();
